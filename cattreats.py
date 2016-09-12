405220563ae707a902bd4ab21577b39fdc7c6ad2
tluoto <toni.luoto@helsinki.fi>
2016-09-12 08:10:09 +0000

    Update cattreats.py
    .index() returns index of chosen item in list. In this code variable SelectedCat is actually already index wanted, so list containing strings with cat names do not include variable SelectedCat, which is supposed to be index of 7th cat in the list ('Snowball II'). So whole CatIndex variable can be removed from code.

--- cattreats.py
+++ cattreats.py
@@ -21,7 +21,7 @@ Cats = ['Garfield', 'Nermal', 'Tom Cat', 'Puss in Boots', 'Hobbes', 'Stimpy', 'S
 Treats = ['Lasagne', 'Praise', 'Mice', 'Power', 'Calvin', 'Fresh kitty litter', 'Small dogs']
 
 # Find location of selected cat
-CatIndex = Cats.index(SelectedCat)
+# CatIndex = Cats.index(SelectedCat)
 
 # Print cat name and favorit treat on screen
 print("The favorite treat of", SelectedCat, "is", Treats[SelectedCat])
